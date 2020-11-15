d3f19d155307f7f12d1fe77d33950b0b259794ca
fabricius1 <fabriciusbr@gmail.com>
2020-11-15 20:54:02 -0300

    Code refactoring (src folder created)

---
+++ src/model.py
@@ -0,0 +1,12 @@
+from src.constants import ERROR_MSG
+
+
+# Create a Model to handle the calculator's operation
+def evaluateExpression(expression):
+    """Evaluate an expression."""
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+
+    return result
